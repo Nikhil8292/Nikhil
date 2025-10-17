f3aa8864740d85f4ea1a0f9e27cedbd9c7a6e5e3
Nikhil8292 <nikhilkumar04916@gmail.com>
2025-10-17 07:01:37 +0530

    Update 0sub.js

--- Priyansh/commands/0sub.js
+++ Priyansh/commands/0sub.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "sub",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+	credits: "SHADOW KINGZ", 
 	description: "hihihihi",
 	commandCategory: "no prefix",
 	usages: "sub",
@@ -12,9 +12,9 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Priyansh rajput")==0 || event.body.indexOf("sub")==0 || event.body.indexOf("subscribe")==0 || event.body.indexOf("Priyansh")==0) {
+	if (event.body.indexOf("NIKHIL RDX")==0 || event.body.indexOf("sub")==0 || event.body.indexOf("subscribe")==0 || event.body.indexOf("Priyansh")==0) {
 		var msg = {
-				body: "👋For Any Kind Of Help Contact On Telegram  Username 👉 @Priyanshrajput😇",
+				body: "👋For Any Kind Of Help Contact On Telegram  Username 👉 @Shadow_king_baby😇",
 				attachment: fs.createReadStream(__dirname + `/noprefix/sub.mp3`)
 			}
 			api.sendMessage(msg, threadID, messageID);
